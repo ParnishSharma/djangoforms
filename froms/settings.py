e6cf327a1ba41824940957938ffc1512b6cf9da5
Parnish Sharma <90691906+ParnishSharma@users.noreply.github.com>
2023-04-28 17:50:19 +0530

    Update settings.py

--- froms/settings.py
+++ froms/settings.py
@@ -28,6 +28,7 @@ DEBUG = True
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.onrender.com']
 
 CORS_ALLOWED_ORIGINS = [    "https://tryforms.onrender.com",    "https://www.tryforms.onrender.com",    "http://localhost:8000",]
+CSRF_TRUSTED_ORIGINS = ['https://tryforms.onrender.com']
 
 # Application definition
 
@@ -45,7 +46,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    #'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
